465c3b6d2be34a1d8d81ea36b0dfc92e701de355
Samuli Koppu <samulikoppi@gmail.com>
2022-01-25 10:51:18 +0200

    Osiot 1.3, 1.4, 1.5 (kurssitiedot valmis)

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -1,44 +1,55 @@
 import React from 'react'
 
 const App = () => {
-  
+ 
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   const Header = (props) => {
     return (
       <>
-        <h1>{props.course}</h1>
+        <h1>{props.name}</h1>
       </>
     )
   }
-  const Part = (props) => {
+  const Content = (props) => {
     return (
       <>
-        <p>
-        {props.part} {props.exercises}
-      </p>
+        <p>{props.kurssi} {props.harjoitukset}</p>
       </>
     )
   }
   const Total = (props) => {
     return (
       <>
-       <p>Number of exercises {props.exercises}</p>
+        <p>Number of exercises {props.yhteensa}</p>
       </>
     )
   }
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
 
   return (
     <div>
-      <Header course='Half Stack application development'/>
-      <Part  part= 'Fundamentals of React' exercises = {exercises1}/>
-      <Part  part= 'Using props to pass data' exercises = {exercises2}/>
-      <Part  part= 'State of a component' exercises = {exercises3}/>
-      <Total exercises = {exercises1+exercises2+exercises3}/>
+      <Header name={course.name}/>
+      <Content kurssi={course.parts[0].name} harjoitukset={course.parts[0].exercises} />
+      <Content kurssi={course.parts[1].name} harjoitukset={course.parts[1].exercises} />
+      <Content kurssi={course.parts[2].name} harjoitukset={course.parts[2].exercises} />
+      <Total yhteensa={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises}/>
     </div>
   )
 }
-
 
 export default App
