f55ce0dfebfbd23835665657dd144951142c8e4e
Samuli Koppi <samulikoppi@gmail.com>
2022-01-31 14:36:03 +0200

    Anekdootit tehty

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,22 +1,66 @@
 import React, { useState } from 'react'
 
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
+    'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
     'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
     'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
     'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
   ]
-   
-  const [selected, setSelected] = useState(0)
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(7).fill(0));
+
+  const handleClick = () => {
+    const randomNumber = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomNumber);
+  }
+
+  const handleVote = () => {
+    const vote = [...votes];
+    vote[selected] += 1;
+    setVotes(vote);
+  }
+
+  //tähän mostVotes piti katsoa vähän apua netistä...
+  const mostVotes = () => {
+    let biggestElement = 0
+    let elementIndex = 0
+
+    votes.forEach((element, index) => {
+      if (element > biggestElement) {
+        biggestElement = element
+        elementIndex = index
+      }
+    })
+    return elementIndex
+  }
 
   return (
-    <div>
-      {anecdotes[selected]}
-    </div>
+    <>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <Button handleClick={handleVote} text="vote" />
+      <Button handleClick={handleClick} text="next anecdote" />
+      <h1>Anecdote with most votes</h1>
+    
+        <>
+          <p>{anecdotes[mostVotes()]}</p>
+          <p>has {votes[mostVotes()]} votes</p>
+        </>
+      
+    </>
   )
 }
 
