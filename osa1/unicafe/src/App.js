97e6ff5bc3ed5acf8adc276f447e79db91742219
Samuli Koppi <samulikoppi@gmail.com>
2022-01-31 09:59:15 +0200

    Unicafe valmis

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -45,7 +45,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = 0
+  
 
   const handleGoodClick = () => {
     setGood(good + 1)
